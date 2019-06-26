a7818cfbf104c0a3b9d6c164b1c7a03bb543a71c
chuanggefighting <17621538916@163.com>
2019-06-27 00:28:23 +0800

    Site updated: 2019-06-27 00:28:22

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Jun 14 2019 00:39:08 GMT+0800 (China Standard Time)
+// build time:Thu Jun 27 2019 00:26:06 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
