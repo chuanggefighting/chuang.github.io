2b255049f8eaf23454c49556c77f74736104261d
chuanggefighting <17621538916@163.com>
2019-07-09 00:29:32 +0800

    Site updated: 2019-07-09 00:29:31

--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Mon Jul 08 2019 00:30:06 GMT+0800 (中国标准时间)
+// build time:Tue Jul 09 2019 00:29:25 GMT+0800 (中国标准时间)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
